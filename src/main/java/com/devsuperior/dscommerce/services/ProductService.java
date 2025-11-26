096887312202c29e0e2daad226189e73ec5a03bc
Samuel Bitencourt <88355342+samuel9395@users.noreply.github.com>
2025-11-26 18:22:10 -0300

    PUT product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -36,12 +36,24 @@ public class ProductService {
     public ProductDTO insert(ProductDTO dto) {
 
         Product entity = new Product();
+        CopyDtoToEntity(dto, entity);
+        entity =  productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id,ProductDTO dto) {
+
+        Product entity = productRepository.getReferenceById(id);
+        CopyDtoToEntity(dto, entity);
+        entity =  productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void CopyDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
-        entity =  productRepository.save(entity);
-        return new ProductDTO(entity);
     }
 }
