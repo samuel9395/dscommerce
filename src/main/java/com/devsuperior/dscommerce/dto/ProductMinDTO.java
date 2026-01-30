65243523b1d89fdcd458c75cfcafed61cefdcc8d
Samuel Bitencourt <samuelb2f@outlook.com>
2026-01-30 12:43:11 -0300

    Criado o ProductMinDTO, para otimizar a busca por produtos. Agora ao fazer a req de produtos, a api retorna somente: id, name, price e imgUrl.

--- src/main/java/com/devsuperior/dscommerce/dto/ProductMinDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductMinDTO.java
@@ -6,16 +6,13 @@ public class ProductMinDTO {
 
     private Long id;
     private String name;
-    private String description;
     private Double price;
     private String imgUrl;
 
     public ProductMinDTO() {}
 
-    public ProductMinDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
         this.name = name;
-        this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
     }
@@ -23,14 +20,12 @@ public class ProductMinDTO {
     public ProductMinDTO(Product entity) {
         id = entity.getId();
         name = entity.getName();
-        description = entity.getDescription();
         price = entity.getPrice();
         imgUrl = entity.getImgUrl();
     }
 
     public Long getId() {return id;}
     public String getName() {return name;}
-    public String getDescription() {return description;}
     public Double getPrice() {return price;}
     public String getImgUrl() {return imgUrl;}
 
