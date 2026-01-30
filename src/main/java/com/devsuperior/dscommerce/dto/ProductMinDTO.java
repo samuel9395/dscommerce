17402ff337fa71ad6ff67a337f47b628983a515c
Samuel Bitencourt <samuelb2f@outlook.com>
2026-01-30 12:54:14 -0300

    fix(dto): corrige ProductMinDTO e remove duplicações após rebase

--- src/main/java/com/devsuperior/dscommerce/dto/ProductMinDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductMinDTO.java
@@ -9,14 +9,6 @@ public class ProductMinDTO {
     private Double price;
     private String imgUrl;
 
-    public ProductMinDTO() {}
-
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.imgUrl = imgUrl;
-    }
-
     public ProductMinDTO(Product entity) {
         id = entity.getId();
         name = entity.getName();
@@ -24,9 +16,19 @@ public class ProductMinDTO {
         imgUrl = entity.getImgUrl();
     }
 
-    public Long getId() {return id;}
-    public String getName() {return name;}
-    public Double getPrice() {return price;}
-    public String getImgUrl() {return imgUrl;}
+    public Long getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
 }
